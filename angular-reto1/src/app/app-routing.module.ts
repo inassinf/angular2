8a7cec683a4bbf9e891e1975014989aa790d0fa6
inassinf <inass.infantes@gmail.com>
2023-05-30 00:09:22 +0200

    dia1 completado

---
+++ angular-reto1/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, importProvidersFrom } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { RegisterComponent } from "./pages/register/register.component";
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
