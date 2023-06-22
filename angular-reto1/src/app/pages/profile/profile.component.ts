65f92e299a04877df1af42af72a41ec57f3a53e5
inassinf <inass.infantes@gmail.com>
2023-06-22 10:59:30 +0200

    probando Profile

--- angular-reto1/src/app/pages/profile/profile.component.ts
+++ angular-reto1/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from "src/app/models/user";
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  public newUser: User;
+
+  constructor(){
+    this.newUser= new User(26, "Inas", "Infantes", "Inass.infantes@gmail.com", "../../../assets/img/sacarosa", "melilla");
+  }
+
+  public newDates(name:string, last_name:string, email:string, photo:string){
+
+    this.newUser.name = name;
+    this.newUser.last_name = last_name;
+    this.newUser.email = email;
+    this.newUser.photo = photo;
+
+  }
+
 
 }
